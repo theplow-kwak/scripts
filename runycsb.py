f0c150fc5fab2820601b1d01165fcc3b40ffe8ad
jeongsoon.kwak <jeongsoon.kwak@sk.com>
2018-06-14 21:42:30 +0900

    Prevent duplication of cat process

--- runycsb.py
+++ runycsb.py
@@ -27,48 +27,48 @@ ycsb_run = './bin/ycsb run rocksdb -s -P workloads/nvme_test -p rocksdb.dir=/med
 col_names = ['lapstime', 'cum_nand_written', 'cum_host_writes', 'cum_nand_erased', 'nand_written', 'host_writes', 'nand_erased', 'waf', 'wai']
 data = pd.DataFrame(columns = col_names)
 
+cmd1 = subprocess.Popen(['echo', sudo_passwd], stdout=subprocess.PIPE)
+cmd3 = subprocess.Popen(['sudo', '-S'] + cat_trace, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+
 for i in range(100):
-	cmd1 = subprocess.Popen(['echo', sudo_passwd], stdout=subprocess.PIPE)
-	cmd2 = subprocess.Popen(['sudo', '-S'] + wai_info, stdin=cmd1.stdout, stdout=subprocess.PIPE)
-	starttime = time.time()
-	start = dict(keyval_exp.findall(str(cmd2.communicate()[0])))
-	for k, v in start.items():
-		start[k] = to_num(v)
-	print("\n start: ", start)
+    cmd1 = subprocess.Popen(['echo', sudo_passwd], stdout=subprocess.PIPE)
+    cmd2 = subprocess.Popen(['sudo', '-S'] + wai_info, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+    starttime = time.time()
+    start = dict(keyval_exp.findall(str(cmd2.communicate()[0])))
+    for k, v in start.items():
+        start[k] = to_num(v)
+    print("\n start: ", start)
 
-	cmd1 = subprocess.Popen(['echo', sudo_passwd], stdout=subprocess.PIPE)
-	cmd3 = subprocess.Popen(['sudo', '-S'] + cat_trace, stdin=cmd1.stdout, stdout=subprocess.PIPE)
-	nvmeparser = subprocess.Popen('python3 /home/dhkwak/projects/traceparser/nvmeparser.py'.split(), stdin=cmd3.stdout)
+    nvmeparser = subprocess.Popen('python3 /home/dhkwak/projects/traceparser/nvmeparser.py'.split(), stdin=cmd3.stdout)
 
-	ycsb = subprocess.Popen(ycsb_run, stdin=subprocess.PIPE)
-	ycsb.wait()
-	time.sleep(10)
-	
-	cmd3.send_signal(signal.SIGINT)
-	nvmeparser.send_signal(signal.SIGINT)
-	nvmeparser.wait()
-		
-	cmd1 = subprocess.Popen(['echo', sudo_passwd], stdout=subprocess.PIPE)
-	cmd2 = subprocess.Popen(['sudo', '-S'] + wai_info, stdin=cmd1.stdout, stdout=subprocess.PIPE)
-	end = dict(keyval_exp.findall(str(cmd2.communicate()[0])))
-	endtime = time.time()
-	for k, v in end.items():
-		end[k] = to_num(v)
-	
-	lapstime = endtime - starttime
-	nand_written = (end['nand_written'] - start['nand_written'])
-	host_writes = (end['host_writes'] - start['host_writes'])
-	nand_erased = (end['nand_erased'] - start['nand_erased'])
-	waf = nand_written / host_writes
-	wai = nand_erased / host_writes
-	data.loc[len(data)] = [lapstime, start['nand_written'], start['host_writes'], start['nand_erased'], nand_written, host_writes, nand_erased, waf, wai]
-	data.to_csv('waf_info.csv')
-		
-	print("end: ", end)
-	print("\n\n", "loop ", i)
-	print("written information: ", nand_written, host_writes, nand_erased)
-	print("WAF = ", waf)
-	print("WAI = ", wai)
-	print("\n\n")
+    ycsb = subprocess.Popen(ycsb_run, stdin=subprocess.PIPE)
+    ycsb.wait()
+    time.sleep(10)
+    
+    nvmeparser.send_signal(signal.SIGINT)
+    nvmeparser.wait()
+        
+    cmd1 = subprocess.Popen(['echo', sudo_passwd], stdout=subprocess.PIPE)
+    cmd2 = subprocess.Popen(['sudo', '-S'] + wai_info, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+    end = dict(keyval_exp.findall(str(cmd2.communicate()[0])))
+    endtime = time.time()
+    for k, v in end.items():
+        end[k] = to_num(v)
+    
+    lapstime = endtime - starttime
+    nand_written = (end['nand_written'] - start['nand_written'])
+    host_writes = (end['host_writes'] - start['host_writes'])
+    nand_erased = (end['nand_erased'] - start['nand_erased'])
+    waf = nand_written / host_writes
+    wai = nand_erased / host_writes
+    data.loc[len(data)] = [lapstime, start['nand_written'], start['host_writes'], start['nand_erased'], nand_written, host_writes, nand_erased, waf, wai]
+    data.to_csv('waf_info.csv')
+        
+    print("end: ", end)
+    print("\n\n", "loop ", i)
+    print("written information: ", nand_written, host_writes, nand_erased)
+    print("WAF = ", waf)
+    print("WAI = ", wai)
+    print("\n\n")
 
 print(data)
