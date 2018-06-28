0c097bbb09196e321c993d948d6f5fb28ad032f6
Unknown <theplow@naver.com>
2018-06-29 08:49:29 +0900

    modularization into class

--- load.sh
+++ load.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+unload=0
+while getopts ":u" opt; do
+	case $opt in
+		u)    unload=1 ;;	
+		\?)   echo "Invalid option: -$OPTARG" >&2 ;;
+		:)    echo "Option -$OPTARG requires an argument." >&2 ;;
+	esac
+done 
+
 pushd ~/linux-4.15.0/drivers/nvme
 sudo rmmod nvme
 sudo rmmod nvme-core
-sudo insmod host/nvme-core.ko streams=1
-sudo insmod host/nvme.ko
+if [ $unload != 1 ]
+then
+    sudo insmod host/nvme-core.ko streams=1
+    sudo insmod host/nvme.ko
+fi
 popd
--- runycsb.py
+++ runycsb.py
@@ -1,89 +1,149 @@
 #!/usr/bin/python3
 
 import re
+import os
 import subprocess
 import signal
 import time
 import getpass
 import pandas as pd
 
-def to_num(str_num):
-    try:
-        return float(str_num) if '.' in str_num else int(str_num, 0)
-    except:
-        return str_num
 
-keyval_exp = re.compile(r'\s?(?P<key>\w+)=(?P<val>[\w|.]+),?')
+nvme_enable = ['echo 1 > /sys/kernel/debug/tracing/events/nvme/enable']
+trace_on = ['echo 1 > /sys/kernel/debug/tracing/tracing_on']
+clear_trace = ['echo 1 > /sys/kernel/debug/tracing/trace']
+stream_on = ['echo 1 > /sys/module/nvme_core/parameters/streams']
+stream_off = ['echo 0 > /sys/module/nvme_core/parameters/streams']
 
-sudo_passwd = getpass.getpass('password:')
-
-nvme_enable = 'echo 1 > /sys/kernel/debug/tracing/events/nvme/enable'
-trace_on = 'echo 1 > /sys/kernel/debug/tracing/tracing_on'
-clear_trace = 'echo 1 > /sys/kernel/debug/tracing/trace'
-stream_on = 'echo 1 > /sys/module/nvme_core/parameters/streams'
-stream_off =  'echo 0 > /sys/module/nvme_core/parameters/streams'
-
+trim = 'fstrim -v /media/unicorn/MultiStream'.split()
 cat_trace = 'cat /sys/kernel/debug/tracing/trace_pipe'.split()
-wai_info = 'nvme hynix wai-information /dev/nvme0'.split()
 
-def sudo_exec(commands):
-    print(commands)
-    echo = subprocess.Popen(['echo', sudo_passwd], stdout=subprocess.PIPE)
-    cmd = subprocess.Popen(commands, stdin=echo.stdout, stdout=subprocess.PIPE)
-    return cmd
+class SudoProcess:
+    sudo_passwd = None
 
-ycsb_load = './bin/ycsb load rocksdb -s -P workloads/nvme_test -p rocksdb.dir=/media/dhkwak/nvme/ycsb-rocksdb-data'.split()
-ycsb_run = './bin/ycsb run rocksdb -s -P workloads/workloada -p rocksdb.dir=/media/dhkwak/nvme/ycsb-rocksdb-data'.split()
+    def __init__(self, shell=False):
+        if shell:
+            self.sudo_cmd = 'sudo -S sh -c'.split()
+        else:
+            self.sudo_cmd = 'sudo -S'.split()
+        if SudoProcess.sudo_passwd is None:
+            SudoProcess.sudo_passwd = getpass.getpass('password:')
 
-def get_wai():
-    cmd1 = subprocess.Popen(['echo', sudo_passwd], stdout=subprocess.PIPE)
-    cmd2 = subprocess.Popen(['sudo', '-S'] + wai_info, stdin=cmd1.stdout, stdout=subprocess.PIPE)
-    _data = dict(keyval_exp.findall(str(cmd2.communicate()[0])))
-    for k, v in _data.items():
-        _data[k] = to_num(v)
-    return _data
+    def Popen(self, commands, wait=True):
+        print(self.sudo_cmd+commands)
+        echo_passwd = subprocess.Popen(['echo', self.sudo_passwd], stdout=subprocess.PIPE)
+        try:
+            cmd = subprocess.Popen(self.sudo_cmd+commands, stdin=echo_passwd.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if wait:
+                cmd.communicate()
+            else:
+                return cmd
+        except:
+            print('process open error! {}'.format(commands))
 
-col_names = ['lapstime', 'cum_nand_written', 'cum_host_writes', 'cum_nand_erased', 'nand_written', 'host_writes', 'nand_erased', 'waf', 'wai']
-data = pd.DataFrame(columns = col_names)
 
-sudo_exec(['sudo', '-S', 'sh', '-c', nvme_enable])
-sudo_exec(['sudo', '-S', 'sh', '-c', stream_on])
-sudo_exec(['sudo', '-S', 'sh', '-c', trace_on])
-sudo_exec(['sudo', '-S', 'sh', '-c', clear_trace])
+class WaiInfo:
+    col_names = ['lapstime', 'cum_nand_written', 'cum_host_writes', 'cum_nand_erased', 'nand_written', 'host_writes',
+                 'nand_erased', 'waf', 'wai']
+    keyval_exp = re.compile(r'\s?(?P<key>\w+)=(?P<val>[\w|.]+),?')
 
-cattrace = sudo_exec(['sudo', '-S'] + cat_trace)
+    def __init__(self, dev='/dev/nvme0'):
+        self.nvme = dev
+        self.__wai_info = 'nvme hynix wai-information {}'.format(self.nvme).split()
+        self.datas = pd.DataFrame(columns=self.col_names)
+        self.index = 0
+        self.last_time = time.time()
+        self.sudo_exec = SudoProcess()
+        self.last_data = self.get_data()
 
-for i in range(100):
-    starttime = time.time()
-    start = get_wai()
-    print("\n start: ", start)
+    def __to_num(self, str_num):
+        try:
+            return float(str_num) if '.' in str_num else int(str_num, 0)
+        except:
+            return str_num
 
-    nvmeparser = subprocess.Popen('python3 /home/dhkwak/projects/traceparser/nvmeparser.py'.split(), stdin=cattrace.stdout)
+    def get_data(self):
+        wei_proc = self.sudo_exec.Popen(self.__wai_info, wait=False)
+        data, err = wei_proc.communicate()
+        if len(data):
+            __data = dict(self.keyval_exp.findall(str(data)))
+            for k, v in __data.items():
+                __data[k] = self.__to_num(v)
+            return __data
+        else:
+            print(err)
 
-    ycsb = subprocess.Popen(ycsb_run, stdin=subprocess.PIPE)
-    ycsb.wait()
-    time.sleep(10)
-    
-    nvmeparser.send_signal(signal.SIGINT)
-    nvmeparser.wait()
-        
-    end = get_wai()
-    endtime = time.time()
-    
-    lapstime = endtime - starttime
-    nand_written = (end['nand_written'] - start['nand_written'])
-    host_writes = (end['host_writes'] - start['host_writes'])
-    nand_erased = (end['nand_erased'] - start['nand_erased'])
-    waf = nand_written / host_writes
-    wai = nand_erased / host_writes
-    data.loc[len(data)] = [lapstime, start['nand_written'], start['host_writes'], start['nand_erased'], nand_written, host_writes, nand_erased, waf, wai]
-    data.to_csv('waf_info.csv')
-        
-    print("end: ", end)
-    print("\n\n", "loop ", i)
-    print("written information: ", nand_written, host_writes, nand_erased)
-    print("WAF = ", waf)
-    print("WAI = ", wai)
-    print("\n\n")
+    def calc(self, end, start=None):
+        if not start:
+            start = self.last_data
+        nand_written = (end['nand_written'] - start['nand_written'])
+        host_writes = (end['host_writes'] - start['host_writes'])
+        nand_erased = (end['nand_erased'] - start['nand_erased'])
+        waf = nand_written / host_writes
+        wai = nand_erased / host_writes
+        return nand_written, host_writes, nand_erased, waf, wai
 
-print(data)
+    def update(self):
+        self.__current = self.get_data()
+
+        if self.__current is not None:
+            __time = time.time()
+            lapstime = __time - self.last_time
+
+            nand_written, host_writes, nand_erased, waf, wai = self.calc(self.__current)
+            self.datas.loc[self.index] = [lapstime, self.last_data['nand_written'], self.last_data['host_writes'], self.last_data['nand_erased'], nand_written, host_writes, nand_erased, waf, wai]
+            self.last_data = self.__current
+            self.last_time = __time
+            self.index += 1
+            return self.__current
+
+#ycsb_workload = 'workloads/nvme_test'
+ycsb_workload = 'workloads/workloadx'
+nvme_path = '/media/dhkwak/nvme'
+
+ycsb_load = './bin/ycsb run rocksdb -s -P {0} -p rocksdb.dir={1}/ycsb-rocksdb-data'.format(ycsb_workload, nvme_path).split()
+ycsb_run = './bin/ycsb run rocksdb -s -P {0} -p rocksdb.dir={1}/ycsb-rocksdb-data'.format(ycsb_workload, nvme_path).split()
+
+if __name__ == "__main__":
+
+    sudo_shell = SudoProcess(True)
+    sudo_shell.Popen(nvme_enable)
+    sudo_shell.Popen(stream_on)
+    sudo_shell.Popen(trace_on)
+    sudo_shell.Popen(clear_trace)
+
+    sudo_exec = SudoProcess()
+    cattrace = sudo_exec.Popen(cat_trace, wait=False)
+
+    for i in range(3):
+        starttime = time.time()
+        wai_info = WaiInfo()
+        start = wai_info.last_data
+
+        outfile = "waf_info" + time.strftime("-%m%d-%H%M") + ".csv"
+        nvmeparser = subprocess.Popen('python3 {}/projects/traceparser/nvmeparser.py'.format(os.getenv("HOME")).split(), stdin=cattrace.stdout)
+        if i == 0:
+            ycsb = subprocess.Popen(ycsb_load)
+        else:
+            ycsb = subprocess.Popen(ycsb_run)
+
+        ticks = 0
+        while ycsb.poll() is None:
+            time.sleep(1)
+            ticks += 1
+            if ticks > 60:
+                end = wai_info.update()
+                wai_info.datas.to_csv(outfile, index=False)
+                ticks = 0
+
+        ycsb.wait()
+        time.sleep(10)
+
+        nvmeparser.send_signal(signal.SIGINT)
+        nvmeparser.wait()
+
+        end = wai_info.update()
+        endtime = time.time()
+        wai_info.datas.to_csv(outfile, index=False)
+        print('lapstime: {}'.format(endtime-starttime), wai_info.calc(end, start))
+
