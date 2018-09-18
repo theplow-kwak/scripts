cce593a605c0c21f1b6ff09a87c8fcdda2d9d38a
Unknown <theplow@naver.com>
2018-09-18 11:39:21 +0900

    add YCSB Shell script

---
+++ YCSB_Test.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# sudo umount /media/unicorn/Gemini960 
+# sudo mount -o defaults,rw,nosuid,nodev,discard /dev/nvme0n1 /media/unicorn/Gemini960
+# sudo chmod 777 /media/unicorn/Gemini960
+
+# iostat -t 60 -md /dev/nvme0n1 > disk.log
+# watch -n 60 "df /dev/nvme0n1 >> df.log"
+
+Init_LOGDirectory(){
+	rm -rf ./LOG
+
+	mkdir LOG
+	mkdir ./LOG/32stream_off/
+	mkdir ./LOG/32stream_on/
+}
+
+Display_TIME_WAI_CAPACITY(){
+	date --date=now
+
+	df -a | grep Filesystem
+	df -a | grep nvme0n1
+
+	sudo nvme hynix wai-information /dev/nvme0n1
+}
+
+RUN_NVMESNOOP(){
+		sudo ~/env/scripts/nvmesnoop.py -o $1 &
+}
+
+KILL_NVMESNOOP(){
+		sleep 3
+		TEST_PID=$(ps aux | grep 'nvmesnoop'| grep -v 'grep' | awk '{print $2}')
+		echo $TEST_PID
+		sudo kill -SIGABRT $TEST_PID
+		sleep 3
+}
+
+Run_YCSB(){
+	echo "=========================================================================================="	
+	echo "                             Test Start                                                   "
+	echo "=========================================================================================="
+	echo "RUN count : $1,  Log_Pos : $2, Thread_Count : $3, Stream_ONOFF : $4"
+	echo "=========================================================================================="
+
+	rm -rf /media/unicorn/Gemini960/*
+#	sudo fstrim -v /media/unicorn/Gemini960
+
+	case $4 in
+		0)
+		sudo -S sh -c 'echo 0 > /sys/module/nvme_core/parameters/streams'
+		echo Set Stream off ; cat /sys/module/nvme_core/parameters/streams
+		;;
+		1)
+		sudo -S sh -c 'echo 1 > /sys/module/nvme_core/parameters/streams'
+		echo Set Stream on ; cat /sys/module/nvme_core/parameters/streams
+		;;
+	esac
+
+	Display_TIME_WAI_CAPACITY
+
+	echo "=========================================================================================="
+
+	for i in $( seq 0 $1 )
+	do
+		case $i in
+			0)
+			echo "=============================================="
+			echo "Load YCSB Data  ***************************"
+			echo "=============================================="		
+#			RUN_NVMESNOOP $2/Load.klog
+
+			`./bin/ycsb load rocksdb -s -P workloads/workloadx -p rocksdb.dir=/media/unicorn/Gemini960/ycsb-rocksdb-data > $2/Load.log 2>&1`
+
+#			KILL_NVMESNOOP
+			
+			Display_TIME_WAI_CAPACITY
+			;;
+			*)
+			echo "==============================================" 
+			echo "RUN YCSB Data $i ***************************"
+			echo "=============================================="		
+#			RUN_NVMESNOOP $2/Run$i.klog
+
+			`./bin/ycsb run rocksdb -s -P workloads/workloadx -threads $3 -p rocksdb.dir=/media/unicorn/Gemini960/ycsb-rocksdb-data > $2/Run$i.log 2>&1`
+
+#			KILL_NVMESNOOP			
+
+			Display_TIME_WAI_CAPACITY
+			
+			grep OVERALL $2/Run$i.log
+			;;
+	   	esac
+
+	echo "=============================================="
+	done
+}
+
+# reset directory
+Init_LOGDirectory
+
+# 12 Testcount Thread 32, stream off
+Run_YCSB 18 ./LOG/32stream_off 32 0
+
+# 12 Testcount Thread 32, stream on
+Run_YCSB 18 ./LOG/32stream_on 32 1
