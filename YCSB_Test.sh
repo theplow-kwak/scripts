beeb809982d42e3eb509ef9b38fa1d2ccb27906f
Unknown <changwan.kim@sk.com>
2018-10-04 17:34:15 +0900

    update SheellScript for monitoring
    add disk space & Performance, modify script flow

--- YCSB_Test.sh
+++ YCSB_Test.sh
@@ -7,141 +7,157 @@
 # iostat -t 60 -md /dev/nvme0n1 > disk.log
 # watch -n 60 "df /dev/nvme0n1 >> df.log"
 
-Init_LOGDirectory(){
-	rm -rf ./LOG
-
-	mkdir LOG
-	mkdir ./LOG/32stream_off/
-	mkdir ./LOG/32stream_on/
+NVME_DEVICE="/dev/nvme0n1"
+DISK_ID=0
+# Clean DB Output & Init
+Clean_Device(){
+	echo Clean Device $1 | tee -a $2
+	
+	rm $1/*
+#	sudo fstrim -v $1
 }
 
-Display_TIME_WAI_CAPACITY(){
-	date --date=now
+# Set Stream on/off
+Set_Stream(){
 
-	df -a | grep Filesystem
-	df -a | grep nvme0n1
-
-	sudo nvme hynix wai-information /dev/nvme0n1
-}
-
-# loging NVMe Driver workload
-Start_NVMESNOOP(){
-		sudo ~/env/scripts/nvmesnoop.py -o $1 &
-}
-
-Stop_NVMESNOOP(){
-		sleep 3
-		TEST_PID=$(ps aux | grep 'nvmesnoop'| grep -v 'grep' | awk '{print $2}')
-		echo $TEST_PID
-		sudo kill -SIGABRT $TEST_PID
-		sleep 3
-}
-
-# loging Device Space Usage
-Start_MonitorDiskSpace(){
-		watch -n1 'df /dev/nvme0n1 | tail -n +2 | tee -a '$1'' &
-}
-
-Stop_MonitorDiskSpace(){
-		TEST_PID=$(ps aux | grep 'watch -n'| grep -v 'grep' | awk '{print $2}')
-		echo $TEST_PID
-		sudo kill -SIGKILL $TEST_PID 2>/dev/null
-		sleep 1
-}
-
-# loging IoStat
-Start_IoStat(){
-		iostat -t 60 -md /dev/nvme0n1 > $1 &
-}
-
-Stop_IoStat(){
-		TEST_PID=$(ps aux | grep 'iostat -t'| grep -v 'grep' | awk '{print $2}')
-		echo $TEST_PID
-		
-		sudo kill -SIGKILL $TEST_PID 2>/dev/null
-		sleep 1
-}
-
-
-Run_YCSB(){
-	echo "=========================================================================================="	
-	echo "                             Test Start                                                   "
-	echo "=========================================================================================="
-	echo "RUN count : $1,  Log_Pos : $2, Thread_Count : $3, Stream_ONOFF : $4, TestDestination : $5 "
-	echo "=========================================================================================="
-
-	rm -rf /media/unicorn/$5/*
-#	sudo fstrim -v /media/unicorn/$5
-
-	case $4 in
+	case $1 in
 		0)
 		sudo -S sh -c 'echo 0 > /sys/module/nvme_core/parameters/streams'
-		echo Set Stream off ; cat /sys/module/nvme_core/parameters/streams
+		echo Set Stream off | tee -a $2 ; cat /sys/module/nvme_core/parameters/streams | tee -a $2
 		;;
 		1)
 		sudo -S sh -c 'echo 1 > /sys/module/nvme_core/parameters/streams'
-		echo Set Stream on ; cat /sys/module/nvme_core/parameters/streams
+		echo Set Stream on | tee -a $2 ; cat /sys/module/nvme_core/parameters/streams | tee -a $2
 		;;
 	esac
+}
 
-	Display_TIME_WAI_CAPACITY
+# output usage of disk, WAI
+Display_TIME_WAI_CAPACITY(){
+	date '+%Y-%m-%d %H:%M:%S' 					| tee -a $1
 
-	echo "=========================================================================================="
+	df -a | grep Filesystem 					| tee -a $1
+	df -a | grep $NVME_DEVICE 					| tee -a $1
 
-	for i in $( seq 0 $1 )
+	sudo nvme hynix wai-information $NVME_DEVICE 			| tee -a $1
+}
+
+Run_DiskSpaceMonitoring(){
+
+	while true
 	do
-		case $i in
-			0)
-			echo "=============================================="
-			echo "Load YCSB Data  ***************************"
-			echo "=============================================="		
-#			Start_NVMESNOOP $2/Load.klog
-#			Start_MonitorDiskSpace $2/Load.dlog
-#			Start_IoStat $2/Load.iolog
-#			echo "check1  ***************************"
-
-			`./bin/ycsb load rocksdb -s -P workloads/workloadx -p rocksdb.dir=/media/unicorn/$5/ycsb-rocksdb-data > $2/Load.log 2>&1`
-
-
-#			Stop_IoStat
-#			Stop_MonitorDiskSpace
-#			Stop_NVMESNOOP
-#			echo "check2  ***************************"
-			
-			Display_TIME_WAI_CAPACITY
-			;;
-
-			*)
-			echo "==============================================" 
-			echo "RUN YCSB Data $i ***************************"
-			echo "=============================================="		
-#			Start_NVMESNOOP $2/Run$i.klog
-#			Start_MonitorDiskSpace $2/Run$i.dlog
-#			Start_IoStat $2/Run$i.iolog
-#			echo "check1  ***************************"
-
-			`./bin/ycsb run rocksdb -s -P workloads/workloadx -threads $3 -p rocksdb.dir=/media/unicorn/$5/ycsb-rocksdb-data > $2/Run$i.log 2>&1`
-
-#			Stop_IoStat
-#			Stop_MonitorDiskSpace
-#			Stop_NVMESNOOP			
-#			echo "check2  ***************************"
-
-			Display_TIME_WAI_CAPACITY
-			
-			grep OVERALL $2/Run$i.log
-			;;
-	   	esac
-
-	echo "=============================================="
+		df $NVME_DEVICE | tail -n +2 >> $1
+		sleep $2
 	done
 }
 
-# reset directory
-Init_LOGDirectory
+Start_BackgroungMonitoring(){
 
-# 12 Step Testcount Thread 32, stream off
-Run_YCSB 3 ./LOG/32stream_off 32 0 Gemini1T9G
+	sudo ~/env/scripts/nvmesnoop.py -o $1 &
 
-# 12 Step Testcount Thread 32, stream on
-Run_YCSB 3 ./LOG/32stream_on 32 1 Gemini1T9G
+	iostat -t 60 -md $NVME_DEVICE > $2 &
+
+	Run_DiskSpaceMonitoring $3 1 &
+
+	DISK_ID=$!
+}
+
+Stop_BackgroungMinitoring(){
+	NVMESNOOP_PID=$(ps aux | grep 'nvmesnoop'| grep -v 'grep' | awk '{print $2}')
+	sudo kill -SIGABRT $NVMESNOOP_PID
+	sleep 1
+		
+	IOSTAT_PID=$(ps aux | grep 'iostat -t'| grep -v 'grep' | awk '{print $2}')
+	sudo kill -SIGABRT $IOSTAT_PID
+	sleep 1
+
+#	WORK_PID=`jobs -l | awk '{print $2}'`
+#	echo "JOBS = $WORK_PID"
+
+	sudo kill -SIGABRT $DISK_ID
+	sleep 1
+}
+
+# Parameter
+#     $1	   $2	       $3		$4		$5		$6
+# Stream offon | RUN Count |Thread count | RockDB Directory | RecordCount | Operation Count
+#
+Run_YCSB(){
+
+# Init Variable
+	LOG_DIR=$(date '+%Y%m%d-%H%M%S')
+	STREAM=$1
+	RUNCNT=$2
+	THREADCNT=$3
+	ROCKSDB_DIR=/media/unicorn/$4/YCSBRocksDB
+	ROCKSDB_LOG_DIR=./$LOG_DIR/$STREAM-Detail
+	RECCNT=$5
+	OPCNT=$6
+	MAIN_LOG=./$LOG_DIR/$LOG_DIR-$STREAM-$RUNCNT.log
+
+# Make LOG
+	mkdir $LOG_DIR	
+
+	mkdir $ROCKSDB_LOG_DIR
+
+# Test Start
+	echo "=========================================================================================="	| tee -a $MAIN_LOG
+	echo "                             Test Start                                                   "	| tee -a $MAIN_LOG
+	echo "=========================================================================================="	| tee -a $MAIN_LOG
+	echo "Environment 1: Stream_ONOFF : $STREAM, RUN count : $RUNCNT,  Thread_Count : $THREADCNT  "		| tee -a $MAIN_LOG
+	echo "Environment 2: TestDestination : $ROCKSDB_DIR, Log_Pos : $ROCKSDB_LOG_DIR "			| tee -a $MAIN_LOG
+	echo "Environment 3: Record Count= $RECCNT,  Operation Count=$OPCNT "					| tee -a $MAIN_LOG
+	echo "=========================================================================================="	| tee -a $MAIN_LOG
+
+	Clean_Device $ROCKSDB_DIR $MAIN_LOG
+
+	Set_Stream $STREAM $MAIN_LOG
+
+	Display_TIME_WAI_CAPACITY $MAIN_LOG
+
+	echo "=========================================================================================="	| tee -a $MAIN_LOG
+
+	for i in $( seq 0 $RUNCNT )
+	do
+		case $i in
+			0)
+			echo "=============================================="					| tee -a $MAIN_LOG
+			echo "Load YCSB Data  ***************************"					| tee -a $MAIN_LOG
+			echo "=============================================="					| tee -a $MAIN_LOG
+			Start_BackgroungMonitoring $ROCKSDB_LOG_DIR/Load.klog $ROCKSDB_LOG_DIR/Load.slog $ROCKSDB_LOG_DIR/Load.dlog
+
+			`./bin/ycsb load rocksdb -s -P workloads/workloadx -p recordcount=$RECCNT -p rocksdb.dir=$ROCKSDB_DIR > $ROCKSDB_LOG_DIR/Load.log 2>&1`
+
+			Stop_BackgroungMinitoring
+			
+			Display_TIME_WAI_CAPACITY $MAIN_LOG
+			;;
+
+			*)
+			echo "==============================================" 					| tee -a $MAIN_LOG
+			echo "RUN YCSB Data $i ***************************"					| tee -a $MAIN_LOG
+			echo "=============================================="					| tee -a $MAIN_LOG
+			Start_BackgroungMonitoring $ROCKSDB_LOG_DIR/Run$i.klog $ROCKSDB_LOG_DIR/Run$i.slog $ROCKSDB_LOG_DIR/Run$i.dlog
+
+			`./bin/ycsb run rocksdb -s -P workloads/workloadx -threads $THREADCNT -p recordcount=$RECCNT -p operationcount=$OPCNT -p rocksdb.dir=$ROCKSDB_DIR > $ROCKSDB_LOG_DIR/Run$i.log 2>&1`
+
+			Stop_BackgroungMinitoring
+
+			Display_TIME_WAI_CAPACITY $MAIN_LOG
+			
+			grep OVERALL $ROCKSDB_LOG_DIR/Run$i.log							| tee -a $MAIN_LOG
+			;;
+	   	esac
+
+	echo "=============================================="							| tee -a $MAIN_LOG
+	done
+}
+
+# Parameter
+#     $1	   $2	       $3		$4		$5		$6
+# Stream offon | RUN Count |Thread count | RockDB Directory | RecordCount | Operation Count
+Run_YCSB 0 1 32 Gemini1T9G 300000 200000
+
+Run_YCSB 1 1 32 Gemini1T9G 300000 200000
+
+
