207ff7c979398b04149fafa095152714f73c37f3
theplow-kwak <54690136+theplow-kwak@users.noreply.github.com>
2022-11-23 07:05:26 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+rm_container()
+{
+    _CONTAINER=$1
+    if [[ $(docker ps -aq --filter "name=^/$_CONTAINER$" --format '{{.Names}}') == $_CONTAINER ]]; then
+        printf "remove container ${_CONTAINER}\n"
+        docker rm $_CONTAINER
+    fi
+}
+
+rm_image()
+{
+    _DOCKERNAME=$1
+    printf "remove docker image ${_DOCKERNAME}\n"
+    # _containers=$(docker ps -aq --filter "ancestor=${_DOCKERNAME}" --format '{{.Names}}')
+    # for _container in $_containers; do
+    #     rm_container ${_container}
+    # done
+    docker rm -f $(docker ps -aq --filter "ancestor=${_DOCKERNAME}" --format '{{.Names}}')
+    docker image rm $_DOCKERNAME
+}
+
+build_image()
+{
+    printf "docker build image ${DOCKERNAME}\n"
+    docker build -t ${DOCKERNAME} --network=host --build-arg NEWUSER=$(whoami) --build-arg NEWUID=$(id -u) $DOCKERPATH || exit 1
+}
+
+docker_run()
+{
+    printf "docker run ${CONTAINER}\n"
+    docker_cmd=(
+        docker run -it --user $(whoami) 
+        -v /etc/ssl/certs:/etc/ssl/certs:ro)
+    [[ -n $SHAREFOLDER ]] && docker_cmd+=(--mount type=bind,source="${SHAREFOLDER}",target=/host)
+    docker_cmd+=(
+        --name "${CONTAINER}" ${DOCKERNAME} /bin/bash)
+
+    echo "${docker_cmd[*]}"
+    ("${docker_cmd[@]}")
+}
+
 usage()
 {
 cat << EOM
@@ -7,55 +48,59 @@ Usage:
  ${0##*/} [OPTIONS] dockerimage
 
 Options:
- -d, --docker   Name of the docker image
- -n, --nvmecli  use nvme-cli source
+ -d, --docker       Path to the docker file
+ -s, --share        Path to the shared folder
+ -c, --container    Name of the container what you want to run
+ -r, --rm           Remove the container
+ -R, --rmi          Remove the docker image and associated containers.
 
 EOM
 }
 
-options=$(getopt -n ${0##*/} -o nd: \
-                --long help,docker:,nvmecli -- "$@")
+options=$(getopt -n ${0##*/} -o s:d:c:rR \
+                --long help,docker:,container:,share,rm,rmi -- "$@")
 [ $? -eq 0 ] || { usage; exit 1; }
 eval set -- "$options"
 
 while true; do
     case $1 in
-        -d | --docker )     DOCKERNAME=${2} ;        shift ;;
-        -n | --nvmecli )    NVMECLI="true" ;;
+        -d | --docker )     DOCKERPATH=${2} ;       shift ;;
+        -s | --share )      SHAREFOLDER=${2} ;      shift ;;
+        -c | --container )  CONTAINER=${2} ;        shift ;;
+        -r | --rm )         removecnt=1 ;;
+        -R | --rmi )        removeimg=1 ;;
         -h | --help )       usage ;                 exit ;;
         --)                 shift ;                 break ;;
     esac
     shift
 done 
 
-DOCKERNAME=${1:-"nvmecli_amd64"}
-
-if [[ -n $NVMECLI ]] ;
-then  
-    SRC_PATH="$HOME/projects/nvme-cli"
-    CONTAINER="${DOCKERNAME}_cont"
+if [[ -n ${DOCKERPATH} ]]; then
+    _tmp=$(realpath ${DOCKERPATH})
+    DOCKERNAME=${_tmp##*/}
 fi
 
-DOCKERFILE="${SRC_PATH}/docker/${DOCKERNAME}/"
-echo $DOCKERFILE
+while (($#)); do
+    DOCKERNAME=$1
+    shift
+done
 
-build_image()
-{
-    printf "docker build image ${DOCKERNAME}\n"
-    docker build -t ${DOCKERNAME} --network=host --build-arg NEWUSER=$(whoami) --build-arg NEWUID=$(id -u) $DOCKERFILE || exit 1
-}
+CONTAINER=${CONTAINER:-"${DOCKERNAME}_cnt"}
 
-docker_run()
-{
-    printf "docker run ${CONTAINER}\n"
-    docker run -it --user $(whoami) \
-        -v /etc/ssl/certs:/etc/ssl/certs:ro \
-        --mount type=bind,source="${SRC_PATH}",target=/works \
-        --name "${CONTAINER}" ${DOCKERNAME} /bin/bash
-}
+if [[ -z ${DOCKERNAME} ]] && [[ "${CONTAINER}" == "_cnt" ]]; then
+    usage
+    docker ps -a
+    exit
+fi
 
-[[ -z $(docker images -q --filter reference=$DOCKERNAME) ]] && build_image
-[[ $(docker ps -aq --filter "name=^/$CONTAINER$" --format '{{.Names}}') == $CONTAINER ]] || { docker_run ; exit ; } 
+[[ $removecnt ]] && rm_container ${CONTAINER}
+[[ $removeimg ]] && rm_image ${DOCKERNAME}
+if [[ $removecnt ]] || [[ $removeimg ]]; then
+    exit
+fi
+
+[[ -n $DOCKERNAME ]] && [[ -z $(docker images -q --filter reference=$DOCKERNAME) ]] && build_image
+[[ $(docker ps -aq --filter "name=^/$CONTAINER$" --format '{{.Names}}') == $CONTAINER ]] || { docker_run ; exit ; }
 [[ $(docker ps --filter "name=^/$CONTAINER$" --format '{{.Names}}') == $CONTAINER ]] || docker start ${CONTAINER}
 
 docker attach ${CONTAINER}
