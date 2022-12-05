b30a135cbd14e1a94873aeea2e44abe815b92ed1
theplow-kwak <54690136+theplow-kwak@users.noreply.github.com>
2022-12-05 10:17:01 +0900

    Update osversion.sh

--- osversion.sh
+++ osversion.sh
@@ -1,32 +1,27 @@
-if [ -f /etc/os-release ]; then
-    # freedesktop.org and systemd
-    . /etc/os-release
-    OS=$NAME
-    VER=$VERSION_ID
-elif type lsb_release >/dev/null 2>&1; then
-    # linuxbase.org
-    OS=$(lsb_release -si)
-    VER=$(lsb_release -sr)
-elif [ -f /etc/lsb-release ]; then
-    # For some versions of Debian/Ubuntu without lsb_release command
-    . /etc/lsb-release
-    OS=$DISTRIB_ID
-    VER=$DISTRIB_RELEASE
-elif [ -f /etc/debian_version ]; then
-    # Older Debian/Ubuntu/etc.
-    OS=Debian
-    VER=$(cat /etc/debian_version)
-elif [ -f /etc/SuSe-release ]; then
-    # Older SuSE/etc.
-    ...
-elif [ -f /etc/redhat-release ]; then
-    # Older Red Hat, CentOS, etc.
-    ...
-else
-    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-    OS=$(uname -s)
-    VER=$(uname -r)
-fi
+#!/bin/bash
 
-echo $OS $VER
+getOsVersion()
+{
+    if [ -f /etc/os-release ]; then
+        # freedesktop.org and systemd
+        . /etc/os-release
+        _OS=$NAME
+        _VER=$VERSION_ID
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        . /etc/lsb-release
+        _OS=$DISTRIB_ID
+        _VER=$DISTRIB_RELEASE
+    elif type lsb_release >/dev/null 2>&1; then
+        # linuxbase.org
+        _OS=$(lsb_release -si)
+        _VER=$(lsb_release -sr)
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        _OS=$(uname -s)
+        _VER=$(uname -r)
+    fi
+}
 
+getOsVersion
+echo $_OS $_VER
