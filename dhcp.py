405a5935288151edca51e0628333de9f1a9746bb
theplow-kwak <54690136+theplow-kwak@users.noreply.github.com>
2022-12-07 10:32:56 +0900

    Update dhcp.py

--- dhcp.py
+++ dhcp.py
@@ -21,15 +21,16 @@ def runshell(cmd, _async=False):
 
 def get_dhcpinfo():
     result = runshell("virsh --quiet net-dhcp-leases default")
-    return result.stdout.rstrip().split('\n') if result.returncode == 0 else []
+    return result.stdout.rstrip().split('\n') if result.returncode == 0 and result.stdout else []
 
 dhcp_leases = get_dhcpinfo()
 
 if len(dhcp_leases) > 0 and len(sys.argv) > 1:
     dhcp_info = dhcp_leases[int(sys.argv[1])].split()
     dest_str = f"<host mac='{dhcp_info[2]}' name='{dhcp_info[5]}' ip='{dhcp_info[4].split('/')[0]}' />"
-    print(f"virsh net-update default delete ip-dhcp-host \"{dest_str}\" --live --config")
-    runshell(f"virsh net-update default delete ip-dhcp-host \"{dest_str}\" --live --config")
+    cmd = f"virsh net-update default delete ip-dhcp-host \"{dest_str}\" --live --config"
+    print(cmd)
+    runshell(cmd)
 else:
     for dhcp in dhcp_leases:
         print(dhcp) 
