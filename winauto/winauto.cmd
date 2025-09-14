4e921b32243c3b4ee7ccf6b87c703efa3df2656a
theplow-kwak <54690136+theplow-kwak@users.noreply.github.com>
2025-09-15 08:34:59 +0900

    Update winauto.cmd

--- winauto/winauto.cmd
+++ winauto/winauto.cmd
@@ -1,12 +1,11 @@
 echo off
 
-set target=Win11_22H2_English_x64v1
-if not [%1]==[] set target=%1
+set srcfolder=.\Win11_24H2
+if not [%1]==[] set srcfolder=%1
 
-set srcfolder=.\ImageSource
+For /F %%A in ("%srcfolder%") do (set target=%%~nxA)
 set targetfile=.\%target%_auto.iso
-
 if not [%2]==[] set targetfile=%2
 
 echo %srcfolder% %targetfile%
-oscdimg.exe -bootdata:2#p0,e,b"%srcfolder%\boot\etfsboot.com"#pEF,e,b"%srcfolder%\efi\Microsoft\boot\efisys.bin" "%srcfolder%" "%targetfile%" -lCCCOMA_X64FRE_EN-US_DV9 -o -h -m -u2
+oscdimg.exe -bootdata:2#p0,e,b"%srcfolder%\boot\etfsboot.com"#pEF,e,b"%srcfolder%\efi\microsoft\boot\efisys.bin" "%srcfolder%" "%targetfile%" -lCCCOMA_X64FRE_EN-US_DV9 -o -h -m -u2
